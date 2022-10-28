556a01cb1a3b0a6abd3e99c0592b4d68f7e2fedd
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-28 12:17:38 -0500

    Actualizacion, objetos

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // Crea al objeto objetoVacio
-
-
+ let objetoVacio = {}
 // Crea al objeto contador
-
-
+let contador = {
+    unidades:1,
+    decenas:5,
+    puntaje:0
+};
 // Crea al objeto bird
-
-
+let bird = {
+    color: "amarillo",
+    poderes:"volar"
+};
 // Crea al objeto juego
-
-
+let juego = {
+    nombre:"Flappy Bird",
+    perdiste:"false"
+}
 // Crea la funcion imprimirUnidades()
-
+function imprimirUnidades(){
+    console.log(contador.unidades)
+}
 
 
